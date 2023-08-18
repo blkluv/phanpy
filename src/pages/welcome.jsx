3e06805f580f3d1929491c31ae921b38601d28cf
blkluv <info@blkluv.org>
2023-08-18 11:11:29 -0400

    welcome

--- src/pages/welcome.jsx
+++ src/pages/welcome.jsx
@@ -1,47 +1,73 @@
 import './welcome.css';
 
+import { Fragment } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
 import boostsCarouselUrl from '../assets/features/boosts-carousel.jpg';
+import luvr from '../assets/features/luvr.png';
+import blue from '../assets/features/blue.png';
 import groupedNotificationsUrl from '../assets/features/grouped-notifications.jpg';
 import multiColumnUrl from '../assets/features/multi-column.jpg';
 import multiHashtagTimelineUrl from '../assets/features/multi-hashtag-timeline.jpg';
 import nestedCommentsThreadUrl from '../assets/features/nested-comments-thread.jpg';
 import logoText from '../assets/logo-text.svg';
 import logo from '../assets/logo.svg';
-import Link from '../components/link';
-import states from '../utils/states';
 import useTitle from '../utils/useTitle';
 
 function Welcome() {
   useTitle(null, ['/', '/welcome']);
+  
   return (
     <main id="welcome">
-      <div class="hero-container">
+      <div className="hero-container">
         <h1>
-          <img
+          <Image
             src={logo}
             alt=""
-            width="200"
-            height="200"
+            width={200}
+            height={200}
             style={{
               aspectRatio: '1/1',
               marginBlockEnd: -16,
             }}
           />
-          <img src={logoText} alt="LUV NFT" width="250" />
+          <Image src={logoText} alt="LUV NFT" width={250} />
         </h1>
         <p>
           <big>
             <b>
-              <Link to="/login" class="button">
-                Log in
+              <Link href="/login">
+                <a className="button">Log in</a>
               </Link>
             </b>
           </big>
         </p>
-        <p class="desc">A positive vibes only social media platform.</p>
+        <p className="desc">A positive vibes only social media platform.</p>
       </div>
       <div id="why-container">
-        <div class="sections">
+        <div className="sections">
+          <section>
+            <img
+              src={luvr}
+              alt="id"
+              loading="lazy"
+            />
+            <h4>LUVR</h4>
+            <p>
+              Your LUV NFT tribe 🆔 (<a href="https://luvr.luvnft.com">Claim it</a>)
+            </p>
+          </section>
+          <section>
+            <img
+              src={blue}
+              alt="blue"
+              loading="lazy"
+            />
+            <h4>Verified</h4>
+            <p>
+              Your verified Blue Check 🔵 (<a href="https://verified.luvnft.com">Claim it</a>)
+            </p>
+          </section>
           <section>
             <img
               src={boostsCarouselUrl}
@@ -99,13 +125,14 @@ function Welcome() {
       </div>
       <hr />
       <p>
-        <a href="https://luvnft.com" target="_blank">
+        <a href="https://luvnft.com" target="_blank" rel="noopener noreferrer">
           Created
         </a>{' '}
         by{' '}
         <a
           href="https://universeodon.com/@luv"
           target="_blank"
+          rel="noopener noreferrer"
           onClick={(e) => {
             e.preventDefault();
             states.showAccount = 'luv@universeodon.com';
@@ -117,6 +144,7 @@ function Welcome() {
         <a
           href="https://github.com/blkluv/phanpy/blob/main/PRIVACY.MD"
           target="_blank"
+          rel="noopener noreferrer"
         >
           Privacy Policy
         </a>
